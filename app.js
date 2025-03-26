1d2833ed5d78811e95f5935a2590cfe46fe54ace
Shivansh Bhardwaj <shivansh2633@gmail.com>
2025-03-26 16:25:41 +0530

    Build nested heading layout with React.createElement

---
+++ app.js
@@ -0,0 +1,23 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement('h1', {
+    id: "title",
+    key:"h 1"
+}, 'heading1 from parcel');
+
+const heading2 = React.createElement('h2', {
+    id: "title",
+    key:"h2"
+}, 'heading2');
+
+const container = React.createElement(
+    "div", { 
+    id: "container",
+},
+    [heading, heading2]
+  );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(container);
