182b538e47ec62838f9e9f3f0adc27d330b2a18f
Shivansh Bhardwaj <shivansh2633@gmail.com>
2025-03-27 17:34:49 +0530

    functional component

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-// Using JSX for both headings
-const heading = <h1 id="title" key="h1">Heading 1  from React</h1>;
-const heading2 = <h1 id="title" key="h2">Heading 2  from React</h1>;
 
-const container = (
-  <div id="container">
-    {heading}
-    {heading2}
-  </div>
-);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+//functional component--------
+const Headercomponent1 = () => {
+  return (
+    <div>
+            <h1>hy from 1</h1>
+    </div>
+  );
+};
+
+const Headercomponent2 = () => {
+  return(
+    <div>
+    <Headercomponent1/>
+    <h1>from react functional component2</h1>
+    <h1>hy from 2</h1>
+  </div> 
+
+  )
+   
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Headercomponent2/>);
+
+
+
+
+
